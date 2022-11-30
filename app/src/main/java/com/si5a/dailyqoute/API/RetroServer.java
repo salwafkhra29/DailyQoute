ca5c2913e7f38f016ea49c0d880b701d8006b70b
salwa.imleta@gmail.com <112602374+salwafkhra29@users.noreply.github.com>
2022-11-30 16:55:58 +0700

    create retrofit.server

---
+++ app/src/main/java/com/si5a/dailyqoute/API/RetroServer.java
@@ -0,0 +1,8 @@
+package com.si5a.dailyqoute.API;
+
+import retrofit2.Retrofit;
+
+public class RetroServer {
+    private static final String baseURL = "https://type.fit/api/";
+    private static Retrofit
+}
