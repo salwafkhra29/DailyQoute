9bb2b66185f2956060c405a8d629f9a366f51606
salwa.imleta@gmail.com <112602374+salwafkhra29@users.noreply.github.com>
2022-11-30 16:51:44 +0700

    make getter setter

---
+++ app/src/main/java/com/si5a/dailyqoute/Model/QuoteModel.java
@@ -0,0 +1,13 @@
+package com.si5a.dailyqoute.Model;
+
+public class QuoteModel {
+    private String text, author;
+
+    public String getText() {
+        return text;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
