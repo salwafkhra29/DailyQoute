8b273647c3444f12d939ada84178ecddd7535f12
salwa.imleta@gmail.com <112602374+salwafkhra29@users.noreply.github.com>
2022-11-30 16:25:13 +0700

    membuat cardview

--- app/src/main/java/com/si5a/dailyqoute/Activity/MainActivity.java
+++ app/src/main/java/com/si5a/dailyqoute/Activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.si5a.dailyqoute;
+package com.si5a.dailyqoute.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.si5a.dailyqoute.R;
 
 public class MainActivity extends AppCompatActivity {
 
